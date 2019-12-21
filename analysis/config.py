16ababd2d8cfe640b528714a68a9a82d78e9b2fb
ashara <anfrankleen23@gmail.com>
2019-12-21 13:14:04 +0000

    modified

---
+++ analysis/config.py
@@ -0,0 +1,6 @@
+file = '/home/ashara/projects/head_first_programming/german_project/welt_nordstream.txt'
+user = "admin"
+password = "pass123!"
+host = "127.0.0.1"
+port = "5432"
+database = "media_comments"
